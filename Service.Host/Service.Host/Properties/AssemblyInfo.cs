714e0138e05338bd1a2d13f65df83a166a5786ed
Nagesh Hugar <Nagesh.Hugar@aptean.com>
2018-06-17 14:20:02 +0530

    Update AssemblyInfo.cs

--- Service.Host/Service.Host/Properties/AssemblyInfo.cs
+++ Service.Host/Service.Host/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Service Host")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Upp Technology, Inc.")]
-[assembly: AssemblyProduct("IRMS 360")]
-[assembly: AssemblyCopyright("Copyright ©  2010")]
+[assembly: AssemblyCompany("SkillsCrafter Technology, Inc.")]
+[assembly: AssemblyProduct("SkillsCrafter")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.6.78")]
-[assembly: AssemblyFileVersion("2.6.78")]
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.0.0")]
